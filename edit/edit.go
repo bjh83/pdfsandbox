93606696839fc808ff717151508cae1754839159
Brendan Higgins <bjh83@case.edu>
2012-11-15 18:18:04 -0500

    NF: I added the changes I lost

--- edit/edit.go
+++ edit/edit.go
@@ -8,28 +8,24 @@ import(
 	"strconv"
 	"compress/flate"
 	"github.com/bjh83/pdfstrip/decode"
-	"fmt"
 )
 
 func replaceText(reader *bufio.Reader, writer io.Writer, newText string) error {
 	endEx, _ := regexp.Compile("endobj")
-	byteBuffer := make([]byte, len(newText) + 11)
-	buffer := bytes.NewBuffer(byteBuffer)
+	buffer := new(bytes.Buffer)
 	tempWriter, err := flate.NewWriter(buffer, 5)
 	if err != nil {
 		return err
 	}
-	fmt.Println(newText[:32])
 	_, err = tempWriter.Write([]byte(newText))
 	err = tempWriter.Close()
 	if err != nil {
 		return err
 	}
-	byteBuffer[0] = byte(120)
-	byteBuffer[1] = byte(156)
-	lengthString := strconv.FormatInt(int64(len(byteBuffer)), 10)
+	lengthString := strconv.FormatInt(int64(buffer.Len() + 2), 10)
 	writer.Write([]byte("<</Length " + lengthString + "/Filter/FlateDecode>>stream\n"))
-	_, err = writer.Write(byteBuffer)
+	_, err = writer.Write([]byte{120, 156})
+	_, err = buffer.WriteTo(writer)
 	_, err = writer.Write([]byte{'\n'})
 	var line string
 	for line, err = reader.ReadString('\n'); err == nil && !endEx.MatchString(line); line, err = reader.ReadString('\n') {}
